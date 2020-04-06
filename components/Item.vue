c96ca2267fd3052d3ba36764b0f4feef8edf3716
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-07 01:54:49 +0900

    家田：カルーセル、アイテムコンポーネントを作成

---
+++ components/Item.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <figure>
+    <img :src="item.imgPath" alt="">
+    <figcaption>{{ item.caption }}</figcaption>
+    <figcaption>{{ item.price }}</figcaption>
+  </figure>
+</div>
+</template>
+
+<style lang="scss">
+figure {
+  display: block;
+  width: 90%;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+  }
+
+  figcaption {
+    text-align: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      item: {
+        imgPath: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        caption: 'ユニコーンのもくもくスタイ',
+        price: '1200'
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,27 +10,31 @@
       md6
     >
       <p><span>トップページ</span>のサンプル文章です</p>
+      <Carousel />
+
+      <v-layout class="u-mt-10">
+        <Item />
+        <Item />
+        <Item />
+      </v-layout>
     </v-flex>
   </v-layout>
 </template>
 
 <style scoped lang="scss">
-div {
-  background-image: url('~assets/img/bg_flower_blue.png');
-  background-size: 100px;
-  background-repeat: repeat;
-}
-
-p {
-  font-size: 30px;
-
-  span {
-    color: red;
-  }
+.u-mt-10 {
+  margin-top: 10px;
 }
 </style>
 
 <script>
+import Carousel from '~/components/Carousel'
+import Item from '~/components/Item'
+
 export default {
+  components: {
+    Carousel,
+    Item
+  }
 }
 </script>
