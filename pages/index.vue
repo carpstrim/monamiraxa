6dde2cb793168ffef8be024e9061a1b49664d6e7
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-10 00:06:57 +0900

    家田：リストレンダリング サンプル作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
       <p><span>トップページ</span>のサンプル文章です</p>
+
+      <div v-for="item in items.items.sty" :key="item.id">
+        <ul>
+          <li>{{ item.name }}</li>
+          <li>{{ item.price }}</li>
+          <li>{{ item.description }}</li>
+        </ul>
+        <br>
+      </div>
       <Carousel />
 
       <v-layout class="u-mt-10">
@@ -35,6 +36,10 @@ export default {
   components: {
     Carousel,
     Item
+  },
+  asyncData ({ store }) {
+    const jsonAll = store.getters['json/getAll']
+    return { items: jsonAll }
   }
 }
 </script>
