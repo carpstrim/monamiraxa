7fa648206f4a9329bcf70ed1f1e376dd7198b2c6
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-12 14:55:06 +0900

    [WIP] 家田：お問い合わせページ開発中

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -4,29 +4,41 @@
     <dl>
       <div>
         <dt>お名前</dt>
-        <dd><v-text-field v-model="a" label="" row-height="15" /></dd>
+        <dd>
+          <v-text-field v-model="contact.name" label="" row-height="15" />
+        </dd>
       </div>
 
       <div>
         <dt>メールアドレス</dt>
-        <dd><v-text-field v-model="b" label="" row-height="15" /></dd>
+        <dd>
+          <v-text-field v-model="contact.mail" label="" row-height="15" />
+        </dd>
       </div>
 
       <div>
         <dt>確認のためにもう一度入力してください</dt>
-        <dd><v-text-field v-model="c" label="" row-height="15" /></dd>
+        <dd>
+          <v-text-field
+            v-model="contact.mailConfirm"
+            label=""
+            row-height="15"
+          />
+        </dd>
       </div>
 
       <div>
         <dt>お問い合わせタイトル</dt>
-        <dd><v-text-field v-model="d" label="" row-height="15" /></dd>
+        <dd>
+          <v-text-field v-model="contact.title" label="" row-height="15" />
+        </dd>
       </div>
 
       <div>
         <dt>内容</dt>
         <dd>
           <v-textarea
-            v-model="e"
+            v-model="contact.content"
             label=""
             value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
             hint="Hint text"
@@ -34,6 +46,54 @@
         </dd>
       </div>
     </dl>
-    <v-btn>送信</v-btn>
+
+    <!-- <v-btn to="/contact/complete" nuxt>
+      送信
+    </v-btn> -->
+
+    <p :class="[isRed ? 'red' : 'blue']">
+      サンプルテキスト
+    </p>
+    <p>{{ isRed }}</p>
+    <p>{{ !isRed }}</p>
+
+    <v-btn @click="doAction">
+      color Cahnge
+    </v-btn>
   </section>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      isRed: false,
+      sending: '',
+      btnStatus: false,
+      contact: {
+        name: '',
+        mail: '',
+        mainConfirm: '',
+        title: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    doAction: () => {
+      console.log(this.isRed)
+      // this.isRed = !this.isRed
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.red {
+  color: red!important;
+}
+
+.blue {
+  color: blue!important;
+}
+</style>
