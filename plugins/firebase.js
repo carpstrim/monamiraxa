4b4b786fe6944db1f58377c9b272d7099af97deb
Kohei Yasumi <k.yasumi.jug@gmail.com>
2020-04-19 21:53:33 +0900

    added firebase tools

---
+++ plugins/firebase.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/functions'
+
+const config = {
+  apiKey: 'AIzaSyCTGtWTZoCPzURVzrFjeeb9oT1aGrJR5to',
+  authDomain: 'monamiraxa-41729.firebaseapp.com',
+  databaseURL: 'https://monamiraxa-41729.firebaseio.com',
+  projectId: 'monamiraxa-41729',
+  storageBucket: 'monamiraxa-41729.appspot.com',
+  messagingSenderId: '1098632777675',
+  appId: '1:1098632777675:web:f57f63cbe65433ef49b04e'
+}
+
+firebase.initializeApp(config)
+
+const firestore = firebase.firestore()
+const auth = firebase.auth()
+const functions = firebase.functions()
+
+export default function (app, inject) {
+  inject('firestore', firestore) // this.$firestore
+  inject('auth', auth) // this.$auth
+  inject('functions', functions)
+}
