22247fadee8f200fb08852e8d2eac5371205cd01
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-06-27 22:36:01 +0900

    [WIP] feat(TOP)：リスを実装中

---
+++ layouts/base.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <main class="main">
+      <v-content>
+        <nuxt />
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
